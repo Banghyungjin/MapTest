4414341095aea347f6a9d2d3be407cfbe2864ee9
HyungJin_Bang <37135305+Banghyungjin@users.noreply.github.com>
2021-06-01 16:03:31 +0900

    Update MainActivity.kt

--- app/src/main/java/com/example/maptest/MainActivity.kt
+++ app/src/main/java/com/example/maptest/MainActivity.kt
@@ -25,30 +25,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        fun returnLocation(){
-            var response_string : ResultGetLocationJson? = null
-            val retrofit = Retrofit.Builder()
-                .baseUrl(BASE_URL_NAVER_API)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-            val api = retrofit.create(NaverReverseGeoAPI::class.java)
-            val callGetLocation = api.getLocation(CLIENT_ID, CLIENT_SECRET, "127.2654387,36.5008113")
-            callGetLocation.enqueue(object : Callback<ResultGetLocationJson> {
-                override fun onResponse(
-                    call: Call<ResultGetLocationJson>,
-                    response: Response<ResultGetLocationJson>
-                ) {
-                    response_string = response.body()
-                    Log.d("결과", "성공 : ${response_string?.results?.size}")
-                    Toast.makeText(this@MainActivity, "${response_string?.results?.get(0)?.region?.area1?.name}", Toast.LENGTH_LONG).show()
-                }
-
-                override fun onFailure(call: Call<ResultGetLocationJson>, t: Throwable) {
-                    Log.d("결과:", "실패 : $t")
-                }
-            })
-        }
-
         val mobile : Button = findViewById(R.id.mobileMap)
         mobile.setOnClickListener {
             val nextIntent = Intent(this, MobileMapActivity::class.java)
@@ -62,14 +38,12 @@ class MainActivity : AppCompatActivity() {
 
         val test : Button = findViewById(R.id.testMap)
         test.setOnClickListener {
+
             val nextIntent3 = Intent(this, MobileMapActivityTest::class.java)
             startActivity(nextIntent3)
 //            returnLocation()
 //            Toast.makeText(this, returnLocation().toString(), Toast.LENGTH_SHORT).show()
         }
-
-
-
 
 
     }
